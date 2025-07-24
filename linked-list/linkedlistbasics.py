fd1ca94658daf366514ceeb2061907cf9ec85a23
Karthik vectrous <karthikn@vectrous.com>
2025-07-24 16:04:58 +0530

    1.added basic operations of linked lists and queues

---
+++ linked-list/linkedlistbasics.py
@@ -0,0 +1,73 @@
+
+class Node:
+    def __init__(self,data,next):
+        self.data=data
+        self.next=next
+
+
+class Linkedlist:
+    def __init__(self):
+        self.head=None
+
+    
+
+    def insertatbeg(self,data):
+        k=Node(data,self.head)
+        self.head=k
+
+    def insertatend(self,data):
+        k=Node(data,None)
+
+        if self.head is None :
+            self.head=k
+            return
+        current=self.head
+        while current.next is not None:
+            current=current.next
+        
+        current.next=k
+
+    
+    def itra(self):
+        current=self.head
+        while current:
+            print(current.data)
+            current=current.next
+
+    def insertatapos(self,data,pos):
+        if pos==0:
+            self.insertatbeg(data)
+            return
+        count=0
+        current=self.head
+        while current is not None and count < pos-1:
+            current = current.next
+            count += 1
+
+        if current is None:
+            print(f"unable to insert as position is not in range")
+            return
+
+        k=Node(data,current.next)
+        current.next=k
+
+
+        
+
+
+
+
+l=Linkedlist()
+l.insertatbeg(10)
+l.insertatbeg(20)
+l.insertatbeg(30)
+l.itra()
+#the above code insert at beg (one before other)
+l.insertatend(3)
+l.insertatend(2)
+l.insertatend(1)
+l.itra()
+#the above code inserts at the end (add after given ones)
+print("\n")
+l.insertatapos(4,88)
+l.itra()
---
+++ stacksandqueues/queues.py
@@ -0,0 +1,21 @@
+from collections import deque
+
+a=deque()
+#queue follows fifo
+
+a.append(1)
+a.append(2)
+a.append(3)
+
+#1----2------3
+
+print(a)
+
+a.pop()
+
+#1---2
+print(a)
+
+#removes first person
+a.popleft()
+print(a)
