84f2b653bc22be564c39e8567c6a0a500cb72016
Karthik vectrous <karthiknarayankkn@gmail.com>
2025-07-29 18:22:13 +0530

    2.modified the insertatbeg and insertatend by DLL

--- linked-list/doublelinkedlist.py
+++ linked-list/doublelinkedlist.py
@@ -1,34 +1,35 @@
 class Node:
-    def __init__(self,data,next=None,prev=None):
+    def __init__(self,data,prev=None,next=None):
         self.data=data
-        self.next=next
         self.prev=prev
+        self.next=next
 
 class Doublelinkedlist:
     def __init__(self):
-        self.head=None
-    
+        self.head=None  
+
     def insertatbeg(self,data):
-        new=Node(data,self.head,None)
+        new=Node(data,None,self.head)
         if self.head is not None:
             self.head.prev=new
         self.head=new
-    
+
     def insertatend(self,data):
         new=Node(data,None,None)
+        cur=self.head
         if self.head is None:
             self.head=new
             return
-        cur=self.head
         while cur.next is not None:
             cur=cur.next
+        #here i have reached the last position and i need to append new node
         cur.next=new
         new.prev=cur
 
     def itra(self):
         cur=self.head
-        if self.head is None:
-            print("List is empty")
+        if cur is None:
+            print("lsit is empty")
             return
         while cur is not None:
             print(cur.data)
@@ -38,4 +39,9 @@ l=Doublelinkedlist()
 l.insertatbeg(10)
 l.insertatbeg(20)
 l.insertatbeg(30)
+l.itra()
+print("\n")
+l.insertatend(5)
+l.insertatend(4)
+l.insertatend(3)
 l.itra()
