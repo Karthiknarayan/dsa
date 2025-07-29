0557afbe6fb2554db570e958f6aa865f6b9c8bc4
Karthik vectrous <karthiknarayankkn@gmail.com>
2025-07-29 18:52:25 +0530

    3.added insertatapos and count functions in DLL

--- linked-list/doublelinkedlist.py
+++ linked-list/doublelinkedlist.py
@@ -7,7 +7,7 @@ class Node:
 class Doublelinkedlist:
     def __init__(self):
         self.head=None  
-
+#insert functions
     def insertatbeg(self,data):
         new=Node(data,None,self.head)
         if self.head is not None:
@@ -26,6 +26,28 @@ class Doublelinkedlist:
         cur.next=new
         new.prev=cur
 
+    def insertatapos(self,pos,data):
+        #1. add at 0th position
+        if pos==0:
+            self.insertatbeg(data)
+            return
+        #2. add at any other position
+        count=0
+        cur=self.head
+        while cur is not None and count<pos-1:
+            cur=cur.next
+            count+=1
+        if cur is None:
+            print("unable to insert at the given position as out of range")
+            return
+        temp=cur.next
+        new=Node(data,cur,cur.next)
+        cur.next=new
+        if temp is not None:
+            temp.prev=new
+
+
+#print or iteration
     def itra(self):
         cur=self.head
         if cur is None:
@@ -35,13 +57,30 @@ class Doublelinkedlist:
             print(cur.data)
             cur=cur.next
 
+#count
+    def count(self):
+        count=0
+        cur=self.head
+        while cur is not None:
+            cur=cur.next
+            count+=1
+        return count
+
 l=Doublelinkedlist()
-l.insertatbeg(10)
-l.insertatbeg(20)
-l.insertatbeg(30)
-l.itra()
-print("\n")
+# l.insertatbeg(10)
+# l.insertatbeg(20)
+# l.insertatbeg(30)
+# l.itra()
+# print("\n")
 l.insertatend(5)
 l.insertatend(4)
 l.insertatend(3)
+# l.itra()
+# print("\n")
+# l.insertatapos(0,200)
+# l.itra()
+
+l.insertatapos(1,200)
+l.insertatapos(4,300)
 l.itra()
+print(f"there are total {l.count()} nodes in the list")
