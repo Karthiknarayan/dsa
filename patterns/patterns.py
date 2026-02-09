ed57fa935ca21a7a1143d84a25872a6bda7c4adb
karthiknarayan <karthiknarayankkn@gmail.com>
2026-02-09 17:50:40 +0530

    4+5 pattern

--- patterns/patterns.py
+++ patterns/patterns.py
@@ -36,6 +36,24 @@ def pattern4(n):
             print(" ",end="")
         print("")
 
+def pattern4n5(n):
+    for i in range(0,n):
+        for j in range(0,n-i):
+            print(" ",end="")
+        for j in range(0,2*i+1):
+            print("*",end="")
+        for j in range(0,n-i):
+            print(" ",end="")
+        print("")
+    for i in range(1,n):
+        for j in range(0,i+1):
+            print(" ",end="")
+        for j in range(0,2*(n-i)-1):
+            print("*",end="")
+        for j in range(0,i):
+            print(" ",end="")
+        print("")
+
 #pattern1(6)
 # 1
 # 12
@@ -58,4 +76,9 @@ def pattern4(n):
 #   *****    
 
 
-pattern4(5)
+pattern4n5(5)
+# *********
+#  ******* 
+#   *****  
+#    ***   
+#     *    
