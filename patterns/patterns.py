708e58e6636a000c7b55f505d54e505ace238d9c
karthiknarayan <karthiknarayankkn@gmail.com>
2026-02-09 16:25:05 +0530

    patterns

---
+++ patterns/patterns.py
@@ -0,0 +1,49 @@
+# 1
+# 12
+# 123
+# 1234
+# 12345
+
+
+def pattern1(n):
+    for i in range(0,n):
+        for j in range(0,i):
+            print(j+1,end="")
+        print("")
+
+def pattern2(n):
+    for i in range(0,n):
+        for j in range(0,n-i):
+            print(j+1,end="")
+        print("")
+
+def pattern3(n):
+    for i in range(0,n):
+        for j in range(0,n-i):
+            print(" ",end="")
+        for k in range (0,2*i+1):
+            print("*",end="")
+        for l in range(0,n-i):
+            print("",end="")
+        print("")
+
+#pattern1(6)
+# 1
+# 12
+# 123
+# 1234
+# 12345
+
+
+#pattern2(5)
+# 12345
+# 1234
+# 123
+# 12
+# 1
+
+pattern3(5)
+ 
+#     *    
+#    ***    
+#   *****    
